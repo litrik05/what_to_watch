6c57579640dfacf3223ffb1dcc8b0820289f6e55
Timur Yurov <litrik05@gmail.com>
2025-09-11 19:34:32 +0300

    fix after flask_api_6

--- opinions_app/error_handlers.py
+++ opinions_app/error_handlers.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import jsonify, render_template
 
 from . import app, db
 
@@ -12,3 +12,21 @@ def internal_error(error):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
+class InvalidAPIUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None):
+        super().__init__()
+        self.message = message
+        if status_code:
+            self.status_code = status_code
+
+    def to_dict(self):
+        return dict(message=self.message)
+
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(error):
+    return jsonify(error.to_dict()), error.status_code
